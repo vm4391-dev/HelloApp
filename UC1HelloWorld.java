3ed54370787d0dbf0c160f7e4e207c02175c726a
vm4391-dev <vm4391@srmist.edu.in>
2026-03-11 15:11:37 +0530

    Add files via upload

---
+++ UC1HelloWorld.java
@@ -0,0 +1,7 @@
+//UC1HelloWorld.java
+public class UC1HelloWorld {
+    public static void main(String[] args) {
+        // Print message to the console
+        System.out.println("Hello World!");
+    }
+}

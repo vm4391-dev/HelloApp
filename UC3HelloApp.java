58ad191d2b16e5cec7950f390611ffc3f44e7651
vm4391-dev <vm4391@srmist.edu.in>
2026-03-17 11:11:09 +0530

    Add files via upload

---
+++ UC3HelloApp.java
@@ -0,0 +1,14 @@
+public class UC3HelloApp {
+    public static void main(String[] args) {
+        
+        String name = "World";
+
+        
+        if (args.length > 0) {
+            name = args[0]; 
+        }
+
+        
+        System.out.println("Hello, " + name + "!");
+    }
+}
